d8178cf748184240f8e15fa8b885ec4b2ed8c2cb
Hannah Sy <hannah_idette_sy@dlsu.edu.ph>
2019-07-19 20:01:15 +0800

    base

--- src/algocom_p3/P3.java
+++ src/algocom_p3/P3.java
@@ -10,5 +10,32 @@ package algocom_p3;
  * @author Hannah Sy
  */
 public class P3 {
+    // Number 1
+    public class Building {
+        public int left;  //the left X-coordinate
+        public int right; //the right X-coordinate
+        public int height; //the height
+    }
     
+    public static void skyline (Building[] B) {
+   //put your code here and the print statements for the output.
+    }
+
+    //Number 2
+    public static void mergesort (int[] A) {
+    //put your code here and the print statements for the output.
+    
+    }
+
+    
+    
+    //Number 3
+    public class Pt {
+        public int x;  //X-coordinate
+        public int y; // Y-coordinate}
+    }
+    public static void closestPair (Pt[] P) {
+    //put your code here and the print statements for the output.
+    }
+
 }
